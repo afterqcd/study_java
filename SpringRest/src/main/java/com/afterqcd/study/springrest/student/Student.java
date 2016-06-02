d212fea99ca51257c512b6cc186fdedbff1673c0
afterqcd <afterqcd@gmail.com>
2016-06-02 10:56:16 +0800

    SpringRest验证来自Request的数据

--- SpringRest/src/main/java/com/afterqcd/study/springrest/student/Student.java
+++ SpringRest/src/main/java/com/afterqcd/study/springrest/student/Student.java
@@ -1,11 +1,16 @@
 package com.afterqcd.study.springrest.student;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+
 /**
  * Created by afterqcd on 16/6/1.
  */
 public class Student {
     private long id;
+    @NotEmpty
     private String name;
+    @Range(min = 0, max = 1024)
     private int age;
 
     public long getId() {
--- SpringRest/src/main/java/com/afterqcd/study/springrest/student/StudentController.java
+++ SpringRest/src/main/java/com/afterqcd/study/springrest/student/StudentController.java
@@ -2,13 +2,15 @@ package com.afterqcd.study.springrest.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,14 +30,21 @@ public class StudentController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public Map<String, Object> create(@RequestBody Student student, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new IllegalArgumentException("Invalid student");
+    public Map<String, Object> create(@Valid @RequestBody Student student,
+                                      BindingResult bindingResult, HttpServletResponse response) {
+        HashMap<String, Object> result = new HashMap<>();
+
+        if (bindingResult.hasFieldErrors()) {
+            response.setStatus(500);
+            result.put("Input student", student);
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                result.put(fe.getObjectName() + "." + fe.getField(), fe.getDefaultMessage());
+            }
+        } else {
+            int id = service.add(student);
+            result.put("Created student with auto-generated id " + id, student);
         }
 
-        int id = service.add(student);
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("Created student with auto-generated id " + id, student);
         return result;
     }
 

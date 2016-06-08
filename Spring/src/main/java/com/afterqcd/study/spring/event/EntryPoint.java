eafb1f2427bc3dca550ddc3f1a2edb0d838439df
afterqcd <afterqcd@gmail.com>
2016-06-08 15:55:43 +0800

    Spring: 添加自定义事件的示例代码

--- Spring/src/main/java/com/afterqcd/study/spring/event/EntryPoint.java
+++ Spring/src/main/java/com/afterqcd/study/spring/event/EntryPoint.java
@@ -1,5 +1,6 @@
 package com.afterqcd.study.spring.event;
 
+import com.afterqcd.study.spring.event.custom.CustomEventPublisher;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -10,11 +11,13 @@ public class EntryPoint {
         AnnotationConfigApplicationContext context
                 = new AnnotationConfigApplicationContext("com.afterqcd.study.spring.event");
         context.registerShutdownHook(); // 程序退出前调用context.doClose,进而发布closed事件
-
         // 发布started事件
         context.start();
         context.getBean(HelloWorld.class).sayHello();
         // 发布stopped事件
         context.stop();
+
+        // 发布自定义事件
+        context.getBean(CustomEventPublisher.class).publish();
     }
 }

59517dbd7bdafff608878fd38ea02f482591531a
afterqcd <afterqcd@gmail.com>
2016-10-11 10:53:41 +0800

    add example code

--- ProtoBuf/src/main/java/com/afterqcd/study/protobuf/Demo.java
+++ ProtoBuf/src/main/java/com/afterqcd/study/protobuf/Demo.java
@@ -2,12 +2,16 @@ package com.afterqcd.study.protobuf;
 
 import com.google.protobuf.GeneratedMessageV3;
 
+import java.util.Arrays;
+
 /**
  * Created by afterqcd on 16/9/26.
  */
 public class Demo {
     public static void main(String[] args) {
-        GeneratedMessageV3 addressBook = AddressBookOuterClass.AddressBook.newBuilder().build();
-        addressBook.toByteArray();
+        GeneratedMessageV3 addressBook = AddressBookOuterClass.AddressBook.newBuilder()
+                .addPerson(PersonOuterClass.Person.newBuilder().setEmail("abc@qq.com"))
+                .build();
+        System.out.println(Arrays.toString(addressBook.toByteArray()));
     }
 }

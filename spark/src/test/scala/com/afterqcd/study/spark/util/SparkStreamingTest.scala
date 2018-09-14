ad4fb2a8c1b35d164b92075b6bda427860238bcd
afterqcd <afterqcd@gmail.com>
2018-09-14 12:31:30 +0800

    refactor spark example

---
+++ spark/src/test/scala/com/afterqcd/study/spark/util/SparkStreamingTest.scala
@@ -0,0 +1,11 @@
+package com.afterqcd.study.spark
+
+import com.holdenkarau.spark.testing.StreamingSuiteBase
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by afterqcd on 2018/9/13.
+  */
+trait SparkStreamingTest extends FlatSpec with Matchers with StreamingSuiteBase {
+
+}
---
+++ spark/src/test/scala/com/afterqcd/study/spark/util/SparkTest.scala
@@ -0,0 +1,10 @@
+package com.afterqcd.study.spark
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by afterqcd on 2016/10/11.
+  */
+trait SparkTest extends FlatSpec with Matchers with SharedSparkContext {
+}
